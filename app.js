fa4f114f6d6d2de389931c409550929d0c852567
harish-github35 <harish.inbox35@gmail.com>
2023-09-10 23:59:27 +0530

    add /user hook test otpless

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const cors = require("cors");
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config/config.env" });
-var cookieParser = require("cookie-parser");
+const cors = require('cors');
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+var cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-const mmiRouter = require("./routes/mmi/mmi.router");
+const mmiRouter = require('./routes/mmi/mmi.router');
 
-app.use("/mmi/api", mmiRouter);
+app.use('/mmi/api', mmiRouter);
+
+app.post('/user', (req, res) => {
+  res.json({ message: 'callback has been called', req });
+});
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("server running on localhost:3001"));
+app.listen(PORT, () => console.log('server running on localhost:3001'));
